e8e729b871020df1d0ad806f56861e551e7ff8e5
Andrii <audeyy@gmail.com>
2016-04-07 19:49:30 +0300

    express server replaced with pure node.js

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
-var express = require('express');
+var http = require('http');
 var fs = require('fs');
-var bodyParser = require('body-parser');
-var tree = require('./tree');
 
-var app = express();
-app.use(express.static('public'));
-app.use(bodyParser.json());
+var server = http.createServer(function(request, response) {
 
-app.get('/tree.json', function(req, res) {
-    res.send(tree);
-});
-
-app.post('/submit', function(req, res) {
-
-    fs.writeFile('tree.json', JSON.stringify(req.body), function(err) {
-      if (err) console.log(err);
-      console.log('It\'s saved!');
-    });
-    res.sendStatus(200);
-
+    if (request.method === "GET") {
+        fs.readFile('./public/' + request.url, function(err, data) {
+            if (!err) {
+                var dotoffset = request.url.lastIndexOf('.');
+                var mimetype = dotoffset == -1 ? 'text/plain' : {
+                    '.html': 'text/html',
+                    '.png': 'image/png',
+                    '.css': 'text/css',
+                    '.json': 'application/json',
+                    '.js': 'text/javascript'
+                }[request.url.substr(dotoffset)];
+                response.setHeader('Content-type', mimetype);
+                response.end(data);
+                console.log(request.url, mimetype);
+            } else {
+                console.log('file not found: ' + request.url);
+                response.writeHead(404, "Not Found");
+                response.end();
+            }
+        });
+    } else if (request.method === "POST") {
+        var body = [];
+        request.on('data', function(chunk) {
+            body.push(chunk);
+        }).on('end', function() {
+            body = Buffer.concat(body).toString();
+            fs.writeFile('./public/tree.json', body, function(err) {
+                if (err) console.log(err);
+                console.log('It\'s saved!');
+            });
+            response.status = "200";
+        });
+        response.end();
+    }
 });
 
 var port = 8000;
-app.listen(port, function() {
+server.listen(port, function() {
     console.log("Listening on port 8000");
 });
